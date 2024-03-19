995c4da946ec5ab5ffe25898c7aa93ac72b1833d
h2moon_son <h2moon_son@naver.com>
2024-03-20 00:05:38 +0900

    .

--- sequence.py
+++ sequence.py
@@ -1,47 +1,57 @@
 n = int(input())
 # [4 3 6 8 7 5 2 1]
 sequence = [int(input()) for _ in range (n)]
-# 1 2 3 4 
-# [4 3 ]pop 2번
-# 1 2 5 6
-# [4 3 6] #pop 1번 
-# 1 2 5 7 8 
-# [4 3 6 8 7 5 2 1]
-finish_flag = False
-for i in range (n-1):
-    if not finish_flag:
-        pivot = sequence[i]
-        cnt = 0
-        for j in range (i+1, n-1):
-            if pivot >= sequence[j]:
-                cnt+=1
-            if cnt == 0:
-                #print(pivot)
-                print("NO")
-                finish_flag = True
-                break
-    else: 
-        break
+sequence2 = []
 num = 0
+
 test_stack = []
+
+append_pop_history = []
 for i in range (n):
     while num < sequence[i]:
-        print("+")
+        #print("+")
+        append_pop_history.append("+")
         num+=1 # 1이어도 0에서 시작, while문에 들어오고 1로 변하고 append
         test_stack.append(num)
-        #print(test_stack)
-    c_seq = i
-    if num == sequence[i]: #이제 같아진 경우 언제까지 pop할지 선택
-        next = sequence[i+1] #다음 수열에 있는 애로 검사 
 
-        tgt = 0
-        for i, x in enumerate(test_stack):
-            if x == next: 
-                tgt = i #인덱스 설정하고 break
-                break
-        for i in range (c_seq - tgt): 
-            test_stack.pop()
-            print("-")
+        #print(test_stack)
+    # if num == sequence[i]: #이제 같아진 경우 언제까지 pop할지 선택
+    #     finish_flag = False
+    #     pop_i = i # 전체 시퀀스 i를 저장 
+    #     sequence2.append(test_stack.pop())
+    #     #print("-") # 맨 위에꺼 일단 뺌
+    #     append_pop_history.append("-")
+    #     while not finish_flag and pop_i+1 <n and test_stack: 
+    #         if test_stack[-1] == sequence[pop_i+1]: 
+    #             sequence2.append(test_stack.pop())
+    #             #print("-") #이때 3빠짐
+    #             append_pop_history.append("-")
+    #             pop_i+=1
+    #         else: finish_flag = True 
+        
+    # 플래그 없애고 개선한 코드 아래와 같음 
+    if test_stack and test_stack[-1] == sequence[i]:
+        pop_i = i
+        while test_stack and pop_i < n and test_stack[-1] == sequence[pop_i]:
+            sequence2.append(test_stack.pop())
+            append_pop_history.append("-")
+            pop_i += 1
+
+flag= 1
 while test_stack: 
-    print("-")
-    test_stack.pop()
+    #print("-")
+    append_pop_history.append("-")
+    sequence2.append(test_stack.pop())
+
+for i in range (n):
+    if sequence[i] == sequence2[i]:
+        pass
+    else: 
+        #print("불일치")
+        flag = 0
+
+if flag: 
+    for char in append_pop_history:
+        print(char)
+else: 
+    print("NO")
